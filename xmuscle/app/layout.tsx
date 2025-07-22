879012741a2a44a5e5d3ed0a24ab3804c84db5f8
Mohamed Ahmed <Moo7md.3bdelaty@gmail.com>
2025-07-22 22:47:21 +0300

    Initialize project, add Hero section, and start basic UI setup

---
+++ xmuscle/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "XMuscle",
+  description: "XMuscle is a modern fitness and bodybuilding platform designed to help users plan workouts, track progress, and achieve their physical goals through an intuitive and interactive interface",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ xmuscle/app/page.tsx
@@ -0,0 +1,13 @@
+import { HeroHighlightDemo  } from "../app/components/Hero";
+
+
+export default function Home() {
+  return (
+    <main className="bg-white dark:bg-black w-screen h-screen">
+    
+        <HeroHighlightDemo  />
+      
+
+    </main>
+  );
+}
